21ce7dfc4f3a36a8808d6d50c45a1739e37dddcf
Omar Alawi <omar.alawi17@gmail.com>
2024-12-16 13:16:15 +0100

    corrected base

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
 import { defineConfig } from "vite";
 export default defineConfig({
-  base: "/https://github.com/Medieinstitutet/fed24d-js-intro-inl-2-quiz-marlyn/",
+  base: "/fed24d-js-intro-inl-2-quiz-marlyn/",
 });
